74904a2a94557caa5ad6ad5d725c4488c23e241b
ALVINOLAYEMI-STUDENT <G00428825@atu.ie>
2024-10-21 09:10:38 +0100

    Added try and catch

--- src/main/java/ie/atu/week6/TryCatch.java
+++ src/main/java/ie/atu/week6/TryCatch.java
@@ -7,20 +7,23 @@ public class TryCatch {
         System.out.println("Please enter a number between 0-9 :");
         Scanner myscan = new Scanner(System.in);
 
-        // Read user input as a String
-        String input = myscan.nextLine();
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+        try {
+            // Read user input as a String
+            String input = myscan.nextLine();
 
-        //Check if the number is between 0 and 9
-        if (myNum >= 0 && myNum <= 9)
-        {
-            System.out.println("You entered " +myNum);
-        }
-        else
-        {
-            System.out.println("Not a valid number");
+            //Parse the input to an integer
+            int myNum = Integer.parseInt(input);
+
+            //Check if the number is between 0 and 9
+            if (myNum >= 0 && myNum <= 9) {
+                System.out.println("You entered " + myNum);
+            } else {
+                System.out.println("Not a valid number");
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+         myscan.close();
         }
     }
 }
